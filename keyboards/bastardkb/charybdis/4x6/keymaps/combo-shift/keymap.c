c6b5ce73ae482b99b9345aaf572f4a3468f29410
dcranford <dgcranford@gmail.com>
2024-09-04 10:53:10 -0400

    adding OSM layers reachable from the combo layer

--- keyboards/bastardkb/charybdis/4x6/keymaps/combo-shift/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/combo-shift/keymap.c
@@ -30,12 +30,15 @@ enum charybdis_keymap_layers {
     LAYER_MIR_NAV,
     LAYER_MIR_NUM,
     LAYER_MIR_MOUSE,
-    
+
     // Media controls on nav layer outer columns
     //LAYER_MIR_MEDIA,
     // Function keys on nav layer top row
     LAYER_MIR_FN,
     //LAYER_POINTER = LAYER_MIR_MOUSE,
+
+    LAYER_OSM_NAV,
+    LAYER_OSM_NUM,
 };
 #define LAYER_POINTER LAYER_MIR_MOUSE
 
@@ -81,6 +84,9 @@ static uint16_t auto_pointer_layer_timer = 0;
 #define NUM_SPC LT(LAYER_MIR_NUM, KC_SPC)
 #define SYM_ENT LT(LAYER_MIR_SYM, KC_ENT)
 #define FUN_DEL LT(LAYER_MIR_FN, KC_DEL)
+
+#define ONAV_BS LT(LAYER_OSM_NAV, KC_BSPC)
+#define ONUM_SPC LT(LAYER_OSM_NUM, KC_SPC)
 
 #define K_THSFT MO(LAYER_THE_SFT)
 
@@ -136,7 +142,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
        KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
-                                  NAV_BS,  KC_TAB,  ALT_APP,    SYM_ENT, NUM_SPC,
+                                  ONAV_BS,  KC_TAB,  ALT_APP,    SYM_ENT, ONUM_SPC,
                                            KC_ESC,  CTL_ESC,    FUN_DEL
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
@@ -247,6 +253,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
 
+  [LAYER_OSM_NAV] = LAYOUT_charybdis_4x6(
+  // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
+        KC_F12,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,      KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,
+  // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
+       KC_MNXT, _______, _______, _______, _______, _______,    KC_INS, KC_HOME,   KC_UP,   KC_END,   KC_PGUP, KC_VOLU,
+  // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
+       KC_MPLY, OSM(KC_LGUI), OSM(KC_LALT), OSM(KC_LCTL), OSM(KC_LSFT), _______,    KC_CAPS, KC_LEFT,   KC_DOWN, KC_RIGHT, KC_PGDN, KC_MUTE,
+  // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
+       KC_MPRV, _______, _______, _______, _______, _______,    REDO,    PASTE,     COPY,    CUT,      UNDO,    KC_VOLD,
+  // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
+                                  _______, _______, _______,    KC_ENT, KC_SPC,
+                                           _______, _______,    KC_DEL
+  //                            ╰───────────────────────────╯ ╰──────────────────╯
+  ),
+
+  [LAYER_OSM_NUM] = LAYOUT_charybdis_4x6(
+  // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
+       KC_TILD, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,    KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS,
+  // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
+       _______, KC_LBRC,   KC_P7, KC_P8,    KC_P9,  KC_RBRC,    _______, _______, _______, _______, _______, _______,
+  // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
+       _______, KC_PPLS,   KC_P4, KC_P5,    KC_P6,  KC_EQL,     _______, OSM(KC_LSFT), OSM(KC_LCTL), OSM(KC_LALT), OSM(KC_LGUI), _______,
+  // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
+       _______, KC_GRV,    KC_P1, KC_P2,    KC_P3,  KC_PSLS,    _______, _______, _______, _______, _______, _______,
+  // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
+                                  KC_P0,  KC_PMNS,  _______,    _______, _______,
+                                          KC_PDOT, _______,    _______
+  //                            ╰───────────────────────────╯ ╰──────────────────╯
+  ),
 };
 // clang-format on
 
